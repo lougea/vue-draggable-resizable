23b5b22e1dd7f667a088d5db8dafed4e77c44852
Louise Brunner <brunner.louise@gmail.com>
2019-09-13 10:21:30 +0200

    creates video components

---
+++ components/Video.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <video
+      ref="video"
+      src="~assets/video/cold.mp4"
+      @click="playPause()"
+      @loadeddata="setDuration"
+    ></video>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      duration: 0,
+      currentTime: 0
+    }
+  },
+  methods: {
+    setDuration() {
+      this.duration = this.$refs.video.duration
+      this.currentTime = this.$refs.video.currentTime
+      this.$emit('duration', this.duration)
+      //   this.$emit('currentTime', this.currentTime)
+    },
+    playPause: function() {
+      if (this.$refs.video.paused === true) {
+        this.$refs.video.play()
+        console.log(this.currentTime)
+      } else {
+        this.$refs.video.pause()
+      }
+    }
+  }
+}
+</script>
